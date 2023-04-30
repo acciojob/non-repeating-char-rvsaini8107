9680e9813be8c68fa38af2b8a3bd6a886070c200
ShriGanesh Saini <67692684+rvsaini8107@users.noreply.github.com>
2023-04-30 13:28:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+//var value= prompt();
+var value= prompt();
+function SingleValue(){
+        var charObj={};
+        for(let i=0;i<value.length;i++){
+                var curchar=value[i];
+                if(charObj[curchar]==undefined)
+                        charObj[curchar]=0;
+                // else
+                        charObj[curchar]++;
+        }
+        for (const key in charObj) {
+               
+                if (charObj[key]==1) {
+                        return key;
+                        
+                }
+                
+        }
+        return 0;
+
+}
+alert(SingleValue())
